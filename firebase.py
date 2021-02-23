d7f5d19e40a677bb09997c074c3f46bd75bb20e8
Kasper4649 <imzxz123456@gmail.com>
2021-02-23 19:18:58 +0800

    [fix] fix firebase document

--- firebase.py
+++ firebase.py
@@ -8,11 +8,11 @@ class Firebase(object):
     def __init__(self):
         cert = json.loads(FIREBASE_CREDENTIALS, strict=False)
         cred = credentials.Certificate(cert)
-        app = firebase_admin.initialize_app(cred)
-        self.db = firestore.client(app)
+        firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
 
 if __name__ == '__main__':
     f = Firebase()
     print(f.db.collection("Twitter").document("nanase").get().to_dict())
-    print(f.db.collection("Twitter").document("nanase").get().to_dict())
-    print(f.db.collection("Twitter").document("nanase").get().to_dict())
+    print(f.db.collection("Twitter").document("nonno").get().to_dict())
+    print(f.db.collection("Twitter").document("modelpress").get().to_dict())
