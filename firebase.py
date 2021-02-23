421dab43b351dd26481a923886c7d8a88a4c30d4
Kasper4649 <imzxz123456@gmail.com>
2021-02-23 19:39:51 +0800

    [feature] add get and set func

--- firebase.py
+++ firebase.py
@@ -11,8 +11,17 @@ class Firebase(object):
         firebase_admin.initialize_app(cred)
         self.db = firestore.client()
 
+    def get(self, collection, document):
+        self.db.collection(collection).document(document).get().to_dict()
+
+    def set(self, collection, document, account):
+        self.db.collection(collection).document(document).set(account)
+
 if __name__ == '__main__':
+    nonno = {
+        "filter_word": "西野七瀬",
+        "last_seen_id": "1363847281406070784",
+        "screen_name": "nonno_staff"
+    }
     f = Firebase()
-    print(f.db.collection("Twitter").document("nanase").get().to_dict())
-    print(f.db.collection("Twitter").document("nonno").get().to_dict())
-    print(f.db.collection("Twitter").document("modelpress").get().to_dict())
+    print(f.set("Twitter", "nonno", nonno))
